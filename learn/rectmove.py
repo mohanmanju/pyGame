3bed2eff239ef2cdc18341c2741548a6f61c229e
mohan31 <31mohan31@gmail.com>
2017-08-03 23:01:08 +0530

    rect move

--- learn/rectmove.py
+++ learn/rectmove.py
@@ -15,7 +15,8 @@ display = pygame.display.set_mode((400,400))
 
 pygame.display.update()
 exit = False
-change = 0
+change_x = 0
+change_y = 0
 while not exit:
 
     for event in pygame.event.get():
@@ -23,10 +24,21 @@ while not exit:
             exit = True
         if event.type == pygame.KEYDOWN:
             if(event.key == pygame.K_LEFT):
-                change = -10
+                change_x = -10
             if(event.key == pygame.K_RIGHT):
-                change = 10
-    snake_x += change
+                change_x = 10
+            if(event.key == pygame.K_UP):
+                change_y = -10
+            if(event.key == pygame.K_DOWN):
+                change_y = 10
+        if(event.type == pygame.KEYUP):
+            if(event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT):
+                change_x = 0
+            if(event.key == pygame.K_UP or event.key == pygame.K_DOWN):
+                change_y = 0
+
+    snake_x += change_x
+    snake_y += change_y
     display.fill(white)
     pygame.draw.rect(display,red,[snake_x,snake_y,50,50])
     pygame.display.update()
